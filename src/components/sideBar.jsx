0a4e16e6022cdd776e1645e286042a5bd8b573a5
Michele <michelerborowski@gmail.com>
2022-06-15 16:13:18 -0300

    Sexto commit

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,35 +1,47 @@
 import styled from "styled-components";
-const SideList = styled.aside`  
-    background-color: transparent;
-    position: absolute;
-    top: 20px;
-    left: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 100%;
-    height: 100%;
-    
+
+const SideList = styled.aside`
+  background-color: transparent;
+  position: absolute;
+  left: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: vh; // area visivel
 `;
 const Lista = styled.li`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding:55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
 `;
 
-const Elementos = styled.button`
+const ElementoVazio = styled.button`
   position: absolute;
-  width: 6.4rem;
-  height: 6.4rem;
+  width: 4.5rem;
+  height: 4.5rem;
   border: 4px solid #004a45;
-  border-radius: 12.8rem;
-  font-size: 4rem;
+  border-radius: 9.8rem;
+  font-size: 3rem;
   font-weight: bold;
   color: #004a45;
   cursor: pointer;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   background-color: #00d68f;
+`;
+
+const ElementoAdicona = styled.button`
+  position: absolute;
+  width: 4.5rem;
+  height: 4.5rem;
+  border: 4px solid #DB2C66;
+  border-radius: 9.8rem;
+  font-size: 3rem;
+  font-weight: bold;
+  cursor: pointer;
+  font-family: "Open Sans", sans-serif;
+  color: #FFFFFF;
+  background-color: #FF3D71;
 `;
 
 function SideBar() {
@@ -37,24 +49,26 @@ function SideBar() {
     <SideList>
       <ul>
         <Lista>
-          <Elementos>?</Elementos>
+          <ElementoVazio>?</ElementoVazio>
         </Lista>
         <Lista>
-          <Elementos>?</Elementos>
+          <ElementoVazio>?</ElementoVazio>
         </Lista>
         <Lista>
-          <Elementos>?</Elementos>
+          <ElementoVazio>?</ElementoVazio>
         </Lista>
         <Lista>
-          <Elementos>?</Elementos>
+          <ElementoVazio>?</ElementoVazio>
         </Lista>
         <Lista>
-          <Elementos>?</Elementos>
+          <ElementoVazio>?</ElementoVazio>
         </Lista>
         <Lista>
-          <Elementos>?</Elementos>
+          <ElementoVazio>?</ElementoVazio>
         </Lista>
-
+        <Lista>
+          <ElementoAdicona>+</ElementoAdicona>
+        </Lista>
       </ul>
     </SideList>
   );
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,47 +1,74 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
-import pageBackground from '../assets/pageBackground.png';
-import ashFront from '../assets/ashFront.png';
-import SideBar from '../components/sideBar';
-
-const Container = styled.div`
+import pageBackground from "../assets/pageBackground.png";
+import ashFront from "../assets/ashFront.png";
+import SideBar from "../components/sideBar";
+import searchTooltip from "../assets/searchTooltip.png";
+const DivBotao = styled.div`
+    width: 300px;
+    height: 300px;
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100%;
-    height: 100%;
 `;
 
-
-const ImageAshFront = styled.img`
-    position: absolute;
-
-    &:hover{
-        border-color: black;
-    }
-    
+const Botao = styled.button`
+opacity:0;
 `;
 
-const DivPage= styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0,0,0,0.35);
-    background-image: url(${pageBackground});
-    background-size: cover;
+const ImageSearch = styled.img`
+  position: relative;
+  opacity: 1;
+  transition: opacity 0.2s linear;
+  
 `;
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
 
-function Map(){
-    return(
-        <DivPage>
-            <SideBar></SideBar>
-            <Container>
-                <ImageAshFront src={ashFront} ></ImageAshFront>  
-            </Container>
-            
-        </DivPage>
-    )
+const ImageAshFront = styled.div`
+//   position: absolute;
+  background-image: url(${ashFront});
+  width: 64px;
+  height: 64px;
+  cursor: pointer;
+&:hover + ${Botao}{
+    opacity: 1;
+    border
+ }
+ 
+`;
+
+const DivPage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.35);
+  background-image: url(${pageBackground});
+  background-size: cover;
+`;
+
+function Map() {
+  return (
+    <DivPage>
+      <SideBar></SideBar>
+      <Container>
+        <DivBotao>
+            <ImageAshFront> </ImageAshFront>
+            <Botao>
+                <ImageSearch src={searchTooltip}></ImageSearch>
+            </Botao>
+          
+        </DivBotao>
+      </Container>
+    </DivPage>
+  );
 }
 
 export default Map;
